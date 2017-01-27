71ccb6531eb2089f8fef2d4007525f69d2988cf9
WilliamSavage <williamsavage@hotmail.com>
2017-01-27 12:27:00 +0000

    adds css to form eyeball

--- main.js
+++ main.js
@@ -19,32 +19,29 @@ function handleOrientation(event) {
   var y    = (event.gamma + 90)/1.8; //-90 to 90
 
 // Contraints on coordinates for marble to remain in box
-  if (x >  95) { x =  95};
+  if (x >  93) { x =  93};
   if (x < 5) { x = 5};
-  if (y >  95) { y =  95};
-  if (y < 8) { y = 8};
+  if (y >  90) { y =  90};
+  if (y < 9) { y = 9};
+
+// Apply position to marble
   marble.style.top  = x + '%';
   marble.style.left = y + '%';
 
-
-
-  let xupper = num(hTop) + 2;
-  let xlower = num(hTop) - 2 ;
+// Rules for getting marble in hole
+  let xupper = num(hTop) + 1;
+  let xlower = num(hTop) - 1;
   let xtrue = (xupper > x && xlower < x);
 
-  let yupper = num(hLeft) + 2;
-  let ylower = num(hLeft) - 2;
-  let ytrue = (xupper > y && xlower < y);
+  let yupper = num(hLeft) + 1;
+  let ylower = num(hLeft) - 1;
+  let ytrue = (yupper > y && ylower < y);
 
+// Things to do WHEN marble in hole
   if(xtrue && ytrue) {
     marble.style.display = 'none';
     window.navigator.vibrate(300);
 
-    const colourBox = (hue) => {
-      const hslaString = `hsla(${hue}, 90%, 60%, 1)`
-      console.log(hslaString);
-      box.style.backgroundColor = hslaString;
-    }
 
     var i = 0, max = 360, cnt = 10;
     const timer = function() {
@@ -59,12 +56,14 @@ function handleOrientation(event) {
 
     setTimeout(() => {
       marble.style.display = 'inline-block';
-      box.style.backgroundColor = 'white';
+      box.style.backgroundColor = 'none';
     }, 1500);
 
   }
+}
 
-  // const output = document.getElementById('output');
-  // output.innerHTML  = "beta : " + x + "\n";
-  // output.innerHTML += "gamma: " + y + "\n";
+// Helper function
+const colourBox = (hue) => {
+  const hslaString = `hsla(${hue}, 90%, 60%, 1)`
+  document.getElementById('game').style.backgroundColor = hslaString;
 }
